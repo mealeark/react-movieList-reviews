5199dfbdad18b119442fa65825d9fa37841f270a
kcloud99 <kylemcleod1@gmail.com>
2018-05-18 08:57:50 -0700

    Add starter files for example data and server

---
+++ server/index.js
@@ -0,0 +1,4 @@
+// ADVANCED CONTENT
+
+// Write basic express server to serve up static files from node server at port 3000
+// Change npm start script to run this server instead of using live-server
--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
+
+// DO NOT CHANGE ANYTHING IN THIS FILE
 import App from "./components/App.jsx";
