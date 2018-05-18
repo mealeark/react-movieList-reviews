ec6df4d5181da088ab27321a0d7c0b94dcb8b0b3
kcloud99 <kylemcleod1@gmail.com>
2018-05-18 09:21:37 -0700

    Add example data from queries

---
+++ exampleData/reviewsExampleData.js
@@ -0,0 +1,49 @@
+const reviewsExampleData = {
+  status: "OK",
+  copyright:
+    "Copyright (c) 2018 The New York Times Company. All Rights Reserved.",
+  has_more: false,
+  num_results: 2,
+  results: [
+    {
+      display_title: "The Avengers",
+      mpaa_rating: "PG-13",
+      critics_pick: 0,
+      byline: "By A.&#160;O. SCOTT",
+      headline: "Superheroes, Super Battles, Super Egos",
+      summary_short:
+        "Joss Whedon’s long-awaited superhero opus “The Avengers” arrives with a big cast, frequent battles and abundant banter among its characters.",
+      publication_date: "2012-05-03",
+      opening_date: "2012-05-04",
+      date_updated: "2017-11-02 04:18:15",
+      link: {
+        type: "article",
+        url:
+          "http://www.nytimes.com/2012/05/04/movies/robert-downey-jr-in-the-avengers-directed-by-joss-whedon.html",
+        suggested_link_text: "Read the New York Times Review of The Avengers"
+      },
+      multimedia: null
+    },
+    {
+      display_title: "The Avengers",
+      mpaa_rating: "PG-13",
+      critics_pick: 0,
+      byline: "JANET MASLIN",
+      headline: "Avengers, the (Movie)",
+      summary_short:
+        "British agents vs. villain with weather machine. Big-time flop.",
+      publication_date: "1998-08-15",
+      opening_date: "1998-08-14",
+      date_updated: "2017-11-02 04:17:55",
+      link: {
+        type: "article",
+        url:
+          "http://www.nytimes.com/movie/review?res=9C01E1D6103AF936A2575BC0A96E958260",
+        suggested_link_text: "Read the New York Times Review of The Avengers"
+      },
+      multimedia: null
+    }
+  ]
+};
+
+export default reviewsExampleData;
